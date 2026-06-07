2d834239b6106293185f081e67699d4784e97929
Naghma <naghmaparveen3651@gmail.com>
2026-06-07 12:13:54 +0530

    Sum of three numbers

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
 public class A{
   public static void main (String args[]){
-    int a=10;b=20;sum;
-    sum=(a+b);
-    System.out.println("The sum of two numbers is: "+sum);
+    int a=10;b=20;c=30;sum;
+    sum=(a+b+c);
+    System.out.println("The sum of three numbers is: "+sum);
     }
   }
